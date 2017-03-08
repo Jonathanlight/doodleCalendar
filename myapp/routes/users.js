75f309122a9e33106aa90fdc14f452b205786f70
axel <axel.bruneaux@epitech.eu>
2017-03-08 11:38:23 +0100

    add routing for user, signup/login/logout

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* Create new User. */
+router.post('/signup', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* Login User. */
+router.get('/login', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* Logout User. */
+router.get('/logout', function(req, res, next) {
   res.send('respond with a resource');
 });
 
