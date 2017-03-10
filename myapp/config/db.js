e3f2aae679b64e9e3d608e8560c5e503200d1f90
axel <axel.bruneaux@epitech.eu>
2017-03-10 12:26:03 +0100

    quick change modules

--- myapp/config/db.js
+++ myapp/config/db.js
@@ -1,9 +1,9 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : ''
-});
-
-module.exports = connection;
+module.exports = {
+	var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+  	host     : 'localhost',
+  	user     : 'root',
+  	password : '',
+  	database : ''
+	});
+}
